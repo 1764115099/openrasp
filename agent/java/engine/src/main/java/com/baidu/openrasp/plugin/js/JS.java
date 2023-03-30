da4845a9f84479a0d16b17d923200129d16f2562
liudexiang <liudexiang@iie.ac.cn>
2023-03-30 11:18:37 +0800

    test-330-1118

--- agent/java/engine/src/main/java/com/baidu/openrasp/plugin/js/JS.java
+++ agent/java/engine/src/main/java/com/baidu/openrasp/plugin/js/JS.java
@@ -52,7 +52,7 @@ import java.util.Map;
 
 public class JS {
     public static final Logger PLUGIN_LOGGER = Logger.getLogger("com.iie.rasp.plugin.js.log");
-    public static final Logger LOGGER = Logger.getLogger(JS.class.getPackage().getName());
+    public static final Logger LOGGER = Logger.getLogger("com.iie.rasp.plugin.js.log");
     public static Object watchId = null;
 
     private static String pluginConfig = "global.checkPoints=['command','directory','fileUpload','readFile','request','requestEnd','sql','sql_exception','sqlSlowQuery','sqlResult','hbaseSqlResult','writeFile','xxe','ognl','deserialization','reflection','webdav','ssrf','include','eval','copy','rename','loadLibrary','ssrfRedirect','deleteFile','mongodb','response','link','jndi','dns'];";
