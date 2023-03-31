46393711e20215e64c1efd361d0eedce199ecbbf
liudexiang <liudexiang@iie.ac.cn>
2023-03-31 10:55:35 +0800

    test-331-1054 hookHbase查询结果

--- agent/java/engine/src/main/java/com/baidu/openrasp/hook/sql/HbaseSQLResultHook.java
+++ agent/java/engine/src/main/java/com/baidu/openrasp/hook/sql/HbaseSQLResultHook.java
@@ -21,11 +21,16 @@ import com.baidu.openrasp.hook.AbstractClassHook;
 import com.baidu.openrasp.plugin.checker.CheckParameter;
 import com.baidu.openrasp.tool.annotation.HookAnnotation;
 import javassist.*;
+import org.apache.hadoop.hbase.Cell;
+import org.apache.hadoop.hbase.CellUtil;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.log4j.Logger;
 import org.apache.log4j.helpers.LogLog;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * @description: Hbase 查询Hook点
@@ -112,24 +117,58 @@ public class HbaseSQLResultHook extends AbstractClassHook {
         }
     }
 
-    public static void getSqlResult(String server, Object[] results) {
-        LOGGER.debug("--------------in HbaseSQLResultHook getSqlResult,server= " + server + "result= " + results[0].toString());
+    public static void getSqlResult(String server, Object[] hookResults) {
+        LOGGER.debug("--------------in HbaseSQLResultHook getSqlResult,server= " + server + "result= " + hookResults[0].toString());
         HashMap<String, Object> params = new HashMap<String, Object>();
         try {
+            Result result = (Result) hookResults[0];
+            List<Cell> cells = result.listCells();
+            HashMap<String, String> results = new HashMap<String, String>();
+
+            // 遍历 KeyValue 实例
+            for (Cell cell : cells) {
+                // 获取列限定符
+                byte[] qualifierBytes = CellUtil.cloneQualifier(cell);
+                String qualifier = Bytes.toString(qualifierBytes);
+
+                // 获取值
+                byte[] valueBytes = CellUtil.cloneValue(cell);
+                String value = Bytes.toString(valueBytes);
+
+                results.put(qualifier,value);
+            }
+
             params.put("server", server);
-            params.put("result", results[0].toString());
+            params.put("result", results);
         }catch (Exception e){
             e.printStackTrace();
         }
         HookHandler.doCheck(CheckParameter.Type.HbaseSQLResult, params);
     }
 
-    public static void checkSqlResult(String server, Object result) {
-        LOGGER.debug("--------------in HbaseSQLResultHook checkSqlResult,server= " + server + ", scannerResult: " + result.toString());
+    public static void checkSqlResult(String server, Object hookResult) {
+        LOGGER.debug("--------------in HbaseSQLResultHook checkSqlResult,server= " + server + ", scannerResult: " + hookResult.toString());
         HashMap<String, Object> params = new HashMap<String, Object>();
         try {
+            Result result = (Result) hookResult;
+            List<Cell> cells = result.listCells();
+            HashMap<String, String> results = new HashMap<String, String>();
+
+            // 遍历 KeyValue 实例
+            for (Cell cell : cells) {
+                // 获取列限定符
+                byte[] qualifierBytes = CellUtil.cloneQualifier(cell);
+                String qualifier = Bytes.toString(qualifierBytes);
+
+                // 获取值
+                byte[] valueBytes = CellUtil.cloneValue(cell);
+                String value = Bytes.toString(valueBytes);
+
+                results.put(qualifier,value);
+            }
+
             params.put("server", server);
-            params.put("result", result.toString());
+            params.put("result", results);
         } catch (Exception e) {
             e.printStackTrace();
         }
